b5c5d0a8ca8a8170398784363c7724bb64a5bc1c
jinli.yl <jinli.yl@alibaba-inc.com>
2025-07-01 12:10:42 +0800

    bugfix

--- beyondagent/module/trainer/ba_ray_trainer.py
+++ beyondagent/module/trainer/ba_ray_trainer.py
@@ -532,11 +532,12 @@ class BeyondAgentRayPPOTrainer(RayPPOTrainer):
 
                             gen_batch_output = self.env_manager.to_dataproto(trajectories)
                             context_time_cost = [x.metadata["context_time_cost"] for x in trajectories if "context_time_cost" in x.metadata]
-                            metrics.update({
-                              "context_cost_avg":   np.mean(context_time_cost),
-                              "context_cost_max":   np.max(context_time_cost),
-                              "context_cost_min":   np.min(context_time_cost),
-                            })
+                            if context_time_cost:
+                                metrics.update({
+                                  "context_cost_avg":   np.mean(context_time_cost),
+                                  "context_cost_max":   np.max(context_time_cost),
+                                  "context_cost_min":   np.min(context_time_cost),
+                                })
 
                             print(f"gen_batch_output.info batch.keys={gen_batch_output.batch.keys()}")
                             num_term_traj = sum([traj.is_terminated  for traj in trajectories])
